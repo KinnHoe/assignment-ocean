1cbe6ed5a8535499574f819121ab299702d72bb7
KH <88728542+KinnHoe@users.noreply.github.com>
2023-09-20 23:47:50 +0800

    add top bar

--- app/src/main/java/com/example/assignment_ocean/MainActivity.kt
+++ app/src/main/java/com/example/assignment_ocean/MainActivity.kt
@@ -2,18 +2,22 @@ package com.example.assignment_ocean
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.assignment_ocean.databinding.ActivityMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    lateinit var topNavBar : MaterialToolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
-
         binding.bottomNavigationView.setOnItemSelectedListener{
             when(it.itemId){
                 R.id.home -> replaceFragment(Home())
@@ -29,7 +33,30 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        topNavBar = findViewById(R.id.topnavbar)
+
+        topNavBar.setNavigationOnClickListener{
+
+        }
+
+        topNavBar.setOnMenuItemClickListener{ menuItem ->
+
+            when(menuItem.itemId){
+                R.id.nav_person -> {
+                    Toast.makeText(this,"link to setting file using intent in MainActivity.kt",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    false
+            }
+        }
+
+
+        }
     }
+
+
 
     private fun replaceFragment(fragment: Fragment){
 
